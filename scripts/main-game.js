6f4b8e04c15998a09af1b92fbeb80aca8d939d45
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-05-27 20:11:00 -0600

    Major refactor of main-game.js
    - Refactor the function scopes to be more aligned with the specs.
    - Provide feedback after each round is played and display the
      current scores.

--- scripts/main-game.js
+++ scripts/main-game.js
@@ -9,6 +9,120 @@ function sample(arr) {
   return arr[Math.random() * arr.length | 0];
 }
 
+// Create the game options array.
+let gameOptions = ["rock", "paper", "scissors"];
+
+/**
+ * Simple logic for a rock, paper, scissors game.
+ * @param {String} x            The first option.
+ * @param {String} y            The second option.
+ * @returns {String}            Result of the match.
+ * @returns {Boolean}           Whether x beat y.
+ */
+function xBeatsy(x, y) {
+  let rockWins = ['rock beats scissors'];
+  let paperWins = ['paper beats scissors'];
+  let scissorsWins = ['scissors beat paper'];
+  let tie = `tie, both players chose ${x}`;
+
+  // Have our final result be returned in the result object.
+  let result = {outcome: '',
+                playerWins: false};
+
+  if (x === 'rock') {
+    // Both rock
+    if (y === 'rock') {
+      result.outcome = tie;
+      result.playerWins = false;
+    }
+    // rock loses to paper
+    else if (y === 'paper') {
+      result.outcome = paperWins;
+      result.playerWins = false;
+    }
+    // rock beats scissors
+    else {
+      result.outcome = rockWins;
+      result.playerWins = true;
+    }
+  }
+
+  else if (x === 'paper') {
+    // paper beats rock
+    if (y === 'rock') {
+      result.outcome = paperWins;
+      result.playerWins = true;
+    }
+    // Both paper
+    else if (y === 'paper') {
+      result.outcome = tie;
+      result.playerWins = false;
+    }
+    // paper loses to scissors
+    else {
+      result.outcome = scissorsWins;
+      result.playerWins = false;
+    }
+  }
+
+  else {
+    // scissors loses to rock
+    if (y === 'rock') {
+      result.outcome = rockWins;
+      result.playerWins = false;
+    }
+    // scissors beats paper
+    else if (y === 'paper') {
+      result.outcome = scissorsWins;
+      result.playerWins = true;
+    }
+    // Both scissors
+    else {
+      result.outcome = tie;
+      result.playerWins = false;
+    }
+  }
+
+  return result;
+}
+
+/**
+ * Play a round of Rock, paper, scissors
+ * @returns {String} Whether or not the player won the game or tied.
+ */
+function playRound() {
+  let computerChoice = sample(gameOptions);
+  let playerChoice = prompt("What would you like to do?").toLowerCase();
+
+  // Only allow one of the game options.
+  while (!(gameOptions.includes(playerChoice))) {
+    playerChoice = prompt("Please choose either rock, paper, or scissors.").toLowerCase();
+  }
+
+  // Use the logic to determine who wins the game
+  let result = xBeatsy(playerChoice, computerChoice);
+
+  // If it's a tie, redo the round from the beginning.
+  if (result.outcome.includes('tie')) {
+    console.log('It was a tie! Play again.');
+    return playRound();
+  }
+
+  return result;
+
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+/**
+ * Display the current score of the game.
+ */
+function showScore() {
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${computerScore}`);
+}
+
 /**
  * Plays a total of numTimes rounds of Rock, Paper, Scissors with a human player
  * against a computer (random) player.
@@ -16,105 +130,28 @@ function sample(arr) {
  * @returns {Number[]}      The final scores for the human player and the computer.
  */
 function playGame(numTimes) {
-  // Create the game options array.
-  let gameOptions = ["rock", "paper", "scissors"];
-
-/**
- * Simple logic for a rock, paper, scissors game.
- * @param {String} x            The first option.
- * @param {String} y            The second option.
- * @returns {Boolean or String} Whether x beats y or they tie.
- */
-  function xBeatsy(x, y) {
-    if (x === 'rock') {
-      // Both rock
-      if (y === 'rock') {
-        return 'tie';
-      }
-      // rock loses to paper
-      else if (y === 'paper') {
-        return false;
-      }
-      // rock beats scissors
-      else {
-        return true;
-      }
-    }
-
-    else if (x === 'paper') {
-      // paper beats rock
-      if (y === 'rock') {
-        return true;
-      }
-      // Both paper
-      else if (y === 'paper') {
-        return 'tie';
-      }
-      // paper loses to scissors
-      else {
-        return false;
-      }
-    }
-
-    else {
-      // scissors loses to rock
-      if (y === 'rock') {
-        return false;
-      }
-      // scissors beats paper
-      else if (y === 'paper') {
-        return true;
-      }
-      // Both scissors
-      else {
-        return 'tie';
-      }
-    }
-  }
-
-  /**
-   * Play a round of Rock, paper, scissors
-   * @returns {Boolean or String} Whether or not the player won the game or tied.
-   */
-  function playRound() {
-    let computerChoice = sample(gameOptions);
-    let playerChoice = prompt("What would you like to do?").toLowerCase();
-
-    // Only allow one of the game options.
-    while (!(gameOptions.includes(playerChoice))) {
-      playerChoice = prompt("Please choose either rock, paper, or scissors.").toLowerCase();
-    }
-    let playerWins = xBeatsy(playerChoice, computerChoice);
-    return playerWins;
-  }
-
-  let playerScore = 0;
-  let computerScore = 0;
 
   for (let i = 0; i < numTimes; i++) {
     roundResult = playRound();
-    if (roundResult !== 'tie') {
-      playerScore += Number(roundResult);
-      computerScore += Number(~roundResult);
-    }
-    // There was a tie, so more rounds need to be played for the best of numTimes.
-    else {
-      alert("It was a tie! Play again!");
-      i -= 1;
-    }
+    console.log(roundResult.outcome[0]);
+
+    // Increment score using Boolean conversion
+    playerScore += Number(roundResult.playerWins);
+    computerScore += Number(!roundResult.playerWins);
+    showScore();
     
   }
 
   if (playerScore > computerScore) {
-    alert("Congratulations! You win!");
+    console.log("Congratulations! You win!");
   }
 
   else if (playerScore < computerScore) {
-    alert("The Computer wins! Better luck next time.");
+    console.log("The Computer wins! Better luck next time.");
   }
 
   else {
-    alert("It was a draw!");
+    console.log("It was a draw!");
   }
 
 }
